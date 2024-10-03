0d65591d69af3b838b1c3ebc13cfe6ec12057945
Dmitry Goloschapov <dmitriy.goloshchapov@codemark.ru>
2024-10-03 15:18:20 +0300

    Initial commit

---
+++ examples/rich-text-editor/composeApp/src/commonMain/kotlin/ru/gimaz/App.kt
@@ -0,0 +1,32 @@
+package ru.gimaz
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import com.mohamedrejeb.richeditor.model.rememberRichTextState
+import com.mohamedrejeb.richeditor.ui.material3.RichTextEditor
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+
+@Composable
+@Preview
+fun App() {
+    MaterialTheme {
+        var showContent by remember { mutableStateOf(false) }
+        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+            val state = rememberRichTextState()
+
+            RichTextEditor(
+                state = state,
+            )
+        }
+    }
+}
